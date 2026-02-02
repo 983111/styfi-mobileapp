96e3d00454d32afb64d30e8f01f287e8a1f24cde
Vishwajeet <vishwajeetadkine705@gmail.com>
2026-02-02 23:43:20 +0530

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,38 +2,24 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
-<<<<<<< HEAD
-    id("com.google.gms.google-services") // This is REQUIRED
-=======
->>>>>>> 9b3456e6285ce023c13c7915bd9d5a11a4f51582
+    // This is REQUIRED for Firebase
+    id("com.google.gms.google-services") 
 }
 
 android {
     namespace = "com.example.styfi"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> 9b3456e6285ce023c13c7915bd9d5a11a4f51582
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-<<<<<<< HEAD
 
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
-=======
-    
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-    
->>>>>>> 9b3456e6285ce023c13c7915bd9d5a11a4f51582
     defaultConfig {
         applicationId = "com.example.styfi"
         minSdk = flutter.minSdkVersion
@@ -41,13 +27,10 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> 9b3456e6285ce023c13c7915bd9d5a11a4f51582
     buildTypes {
         release {
+            // TODO: Replace with your own signing config for production
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -55,8 +38,4 @@ android {
 
 flutter {
     source = "../.."
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 9b3456e6285ce023c13c7915bd9d5a11a4f51582
